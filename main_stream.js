16b32673d5f557473ef509d5ae2dca613998f38f
haoyi-duan <howie@zju.edu.cn>
2024-12-14 03:04:18 -0800

    update 7777

--- main_stream.js
+++ main_stream.js
@@ -186,9 +186,8 @@ const update_displayed_info = (camera) => {
 };
 
 function connectToServer() {
-    // socket = io.connect('http://localhost:7777/');
-    // socket = io.connect('http://10.79.12.218:7776/');
-    socket = io.connect('http://localhost:8000/');
+    socket = io.connect('http://localhost:7777/');
+    // socket = io.connect('http://localhost:8000/');
 
     socket.on('connect', () => {
         console.log("Connected to server.");
